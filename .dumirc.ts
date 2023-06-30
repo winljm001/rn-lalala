4ee9084b2ce4a9492f267e24e3de98261195b6c5
winljm001 <819648983@qq.com>
2023-06-30 17:33:02 +0800

    feat: doc

--- .dumirc.ts
+++ .dumirc.ts
@@ -23,6 +23,8 @@ export default defineConfig({
   title: "dumi",
   logo: "https://avatars.githubusercontent.com/u/26362957?s=400&u=f0e02a46e137a141c483798140f5b649795c3ea0&v=4",
   outputPath: "docs-dist",
+  base: `/rn-lalala-doc/`,
+  publicPath: `/rn-lalala-doc/`,
   resolve: {
     docDirs: ["docs"],
     atomDirs: [{ type: "component", dir: "packages" }],
